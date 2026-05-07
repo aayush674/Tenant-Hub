68a8014eb2fb6a4ddd3e2fda8377c7c6693b5e4d
aayush674 <aayushkhanna0201@gmail.com>
2026-05-08 02:44:10 +0530

    Handled form validation on UI, Added error box

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -9,22 +9,50 @@ function Login() {
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const [showError, setShowError] = useState(false);
+
+    function triggerError(message) {
+        setShowError(false);
+        setTimeout(() => {
+            setError(message);
+            setShowError(true);
+        }, 100);
+    }
 
     async function handleSubmit(e) {
         e.preventDefault();
 
-        setLoading(true);
-        const data = await login(email, password);
-        if (data.access && data.refresh) {
-            // alert("Login successful!");
-            localStorage.setItem("access_token", data.access);
-            // console.log("Access Token Stored:", data.access);
-            localStorage.setItem("refresh_token", data.refresh);
-            // console.log("Refresh Token Stored:", data.refresh);                
-            navigate("/");
+        if (!email.trim() || !password.trim()) {
+            triggerError("Please enter email and password");
+            setLoading(false);
+            return;
         }
-        else {
-            alert("Login failed. Please check your credentials and try again.");
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            triggerError("Please enter a valid email address");
+            setLoading(false);
+            return;
+        }
+
+        setLoading(true);
+        try {
+            const data = await login(email, password);
+            if (data.access && data.refresh) {
+                // alert("Login successful!");
+                localStorage.setItem("access_token", data.access);
+                // console.log("Access Token Stored:", data.access);
+                localStorage.setItem("refresh_token", data.refresh);
+                // console.log("Refresh Token Stored:", data.refresh);                
+                navigate("/");
+            }
+            else {
+                triggerError("Invalid credentials");
+            }
+        }
+        catch (error) {
+            triggerError("Login failed. Please try again.");
         }
         setLoading(false);
     }
@@ -43,38 +71,36 @@ function Login() {
     return (
         <div className="login-container">
             <div className="login-left">
-                <img src= {complogo} alt="Tenant Hub" className="logo" />
+                <img src={complogo} alt="Tenant Hub" className="logo" />
                 <h1 className="brand-tagline">Manage Tenants. Simplify Living.</h1>
 
-            </div>  
+            </div>
             <div className="login-right">
-            <form className="login-card" onSubmit={handleSubmit}>
+                <form className="login-card" onSubmit={handleSubmit} noValidate>
 
-                <h2 className="login-title">Already a Member? Please proceed.</h2>
+                    <h2 className="login-title">Already a Member? Please proceed.</h2>
+                    <div className={`login-error-container ${showError ? "show" : ""}`}>{error || ""}</div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="login-input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="login-input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="login-input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
 
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="login-input"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
+                    <button type="submit" className="login-button" disabled={loading}>
+                        {loading ? "Logging in..." : "Login"}
+                    </button>
 
-                <button type="submit" className="login-button" disabled={loading}>
-                    {loading ? "Logging in..." : "Login"}
-                </button>
-
-            </form>
+                </form>
             </div>
         </div>
     );
