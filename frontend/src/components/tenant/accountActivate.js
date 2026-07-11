c9afb640d337d2fbc041dbf35c6694f504958be5
aayush674 <aayushkhanna0201@gmail.com>
2026-07-11 23:25:39 +0530

    Fixed authorization issue in activate account api and created wait page after activating

--- frontend/src/components/tenant/accountActivate.js
+++ frontend/src/components/tenant/accountActivate.js
@@ -10,10 +10,12 @@ function AccountActivate() {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const [countdown, setCountdown] = useState(5);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(password.trim()==="" || confirmPassword.trim()===""){
+        if (password.trim() === "" || confirmPassword.trim() === "") {
             setError("Both fields are required to activate account");
             return;
         }
@@ -22,7 +24,7 @@ function AccountActivate() {
             return;
         }
 
-        const response = await authFetch(
+        const response = await fetch(
             "http://localhost:8000/auth/activate/",
             {
                 method: "POST",
@@ -37,12 +39,48 @@ function AccountActivate() {
 
         const data = await response.json();
         if (response.ok) {
-            navigate("/login");
+            setSuccess("Account Activated successfully!");
+            setError("");
+            let time = 5;
+            setCountdown(time);
+
+            const interval = setInterval(() => {
+                time--;
+                setCountdown(time);
+
+                if (time === 0) {
+                    clearInterval(interval);
+                    navigate("/login");
+                }
+            }, 1000);
         }
         else {
             setError(data.detail);
         }
     };
+
+    if (success) {
+        return (
+            <div className="account-activate-container">
+                <div className="activation-success">
+                    <h2>🎉 Account Activated!</h2>
+                    <p>
+                        Your account has been activated successfully.
+                    </p>
+                    <p>
+                        Redirecting to login in {countdown} seconds...
+                    </p>
+
+                    <button
+                        className="activate-button"
+                        onClick={() => navigate("/login")}
+                    >
+                        Go to Login
+                    </button>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="account-activate-container">
